02600971df503489ad4730970c638716c7bdad1b
Dommeti Raja <v-dommraja@microsoft.com>
2025-08-06 14:36:42 +0530

    Refactor Get-AdministrativeUnitMemberships function to enhance member selection and logging

--- SDS Scripts/Get-All_Schools_and_Memberships.ps1
+++ SDS Scripts/Get-All_Schools_and_Memberships.ps1
@@ -81,24 +81,25 @@ if ($skipDownloadCommonFunctions -eq $false) {
 . .\common.ps1
 
 function Get-AdministrativeUnitMemberships($refreshToken, $graphscopes, $logFilePath) {
-
+ 
     #Preparing uri string
-    $auSelectClause = "`$select=id,displayName,description"
-    $auMemberSelectClause = "`$select=id,displayName,mail,@data.type,extension_fe2174665583431c953114ff7268b7b3_Education_SyncSource_SchoolId"
+    $auMemberAllSelectClause = "`$select=id,DisplayName,@data.type,extension_fe2174665583431c953114ff7268b7b3_Education_ObjectType"
 
-    $initialSDSSchoolAUsUri = "$graphEndPoint/$graphVersion/directory/administrativeUnits?`$filter=extension_fe2174665583431c953114ff7268b7b3_Education_ObjectType%20eq%20'School'&$auSelectClause"
-    
+    $initialSDSSchoolAUsUri = "$graphEndPoint/$graphVersion/directory/administrativeUnits?$auMemberAllSelectClause"
+
     #Getting AUs for all schools
     $checkedSDSSchoolAUsUri = TokenSkipCheck $initialSDSSchoolAUsUri $logFilePath
     $allSchoolAUs = PageAll-GraphRequest $checkedSDSSchoolAUsUri $refreshToken 'GET' $graphscopes $logFilePath
-
+ 
     #Write to school AU count to log
-    Write-Output "[$(get-date -Format G)] Retrieve school AUs." | out-file $logFilePath -Append
-
+    Write-Output "[$(Get-Date -Format G)] Retrieve $($allSchoolAUs.Count) school AUs." | out-file $logFilePath -Append
+   
     $schoolAUMemberships = @() #Array of objects for memberships
 
+    $auMemberSelectClause = $auMemberAllSelectClause
+ 
     $i = 0 #Counter for progress
-    
+   
     #Looping through all school Aus
     foreach($au in $allSchoolAUs)
     {
@@ -109,23 +110,21 @@ function Get-AdministrativeUnitMemberships($refreshToken, $graphscopes, $logFile
             $auMembershipUri = $graphEndPoint + '/' + $graphVersion + '/directory/administrativeUnits/' + $au.id + '/members'
             $checkedAUMembershipUri = TokenSkipCheck $auMembershipUri $logFilePath
             $schoolAUMembers = PageAll-GraphRequest $checkedAUMembershipUri $refreshToken 'GET' $graphscopes $logFilePath
-
+ 
             #Getting info for each au member
             foreach ($auMember in $schoolAUMembers)
             {
                 $auMemberType = $auMember.'@odata.type' #Some members are users and some are groups
-                
-                if ($auMemberType -eq '#microsoft.graph.user')
+               
+                if ($auMemberType -eq '#microsoft.graph.user' -or $auMemberType -eq '#microsoft.graph.group')
                 {
-                    $userUri = $graphEndPoint + "/$graphVersion/users/" + $auMember.Id + "?$auMemberSelectClause"
-                    $user = invoke-graphrequest -Method GET -Uri $userUri -ContentType "application/json"
-
+                    
                     #Users created by sds have this extension
-                    if ($user.extension_fe2174665583431c953114ff7268b7b3_Education_SyncSource_SchoolId -ne $null)
-                    {                        
-                        #Create object required for export-csv and add to array
-                        $obj = [pscustomobject]@{"DisplayName"=$au.displayName;"Description"=$au.description;"AUObjectID"=$au.id;"MemberEmailAddress"=$user.mail;"MemberObjectID"=$user.id;}
-                        $schoolAUMemberships += $obj
+                    if ($auMember.extension_fe2174665583431c953114ff7268b7b3_Education_ObjectType -ne $null)
+                    {  
+                            #Create object required for export-csv and add to array
+                            $obj = [pscustomobject]@{"DisplayName"=$au.displayName;"Description"=$au.description;"AUObjectId"=$au.Id;"MemberObjectID"=$auMember.Id; "MemberDisplayName"=$auMember.DisplayName;"MemberEmailAddress"=$auMember.mail}
+                            $schoolAUMemberships += $obj
                     }
                 }
             }
@@ -133,7 +132,7 @@ function Get-AdministrativeUnitMemberships($refreshToken, $graphscopes, $logFile
         $i++
         Write-Progress -Activity "Retrieving school AU memberships" -Status "Progress ->" -PercentComplete ($i/$allSchoolAUs.count*100)
     }
-
+ 
     return $schoolAUMemberships
 }
 
@@ -155,6 +154,7 @@ Function Format-ResultsAndExport($graphscopes, $logFilePath) {
     else {
         Write-Output "[$(get-date -Format G)] There are no AU memberships available." | out-file $logFilePath -Append
     }
+        Write-Host "CSV has been created"
 }
 
 #Main
